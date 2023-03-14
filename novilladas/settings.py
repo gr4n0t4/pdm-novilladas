2a7b030e334249d1c165b8bded676d6780b59802
gr4n0t4 <gr4n0t4@hotmail.com>
2023-03-14 09:55:00 +0000

    Changes to work with proxy

--- novilladas/settings.py
+++ novilladas/settings.py
@@ -14,18 +14,22 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+STATIC_ROOT = 'static'
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/s
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+FORCE_SCRIPT_NAME = '/pdm'
+BASE_URL='https://gr4n0t4.ddns.net'
+USE_X_FORWARDED_HOST = True
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['https://gr4n0t4.ddns.net']
+ALLOWED_HOSTS = ['https://gr4n0t4.ddns.net', '*']
 
 
 # Application definition
